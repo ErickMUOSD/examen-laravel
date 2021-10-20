6098841f82ce17f49ab76f26865ecf9296bd0346
Erick Sanabria <martinoli944@gmail.com>
2021-10-20 10:26:38 -0500

    Migracion poliza

---
+++ database/migrations/2021_10_20_151845_create_polizas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePolizasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polizas', function (Blueprint $table) {
+            $table->bigIncrements('nropoliza');
+            $table->UnsigenedBigInteger('patente');
+            $table->UnsigenedBigInteger('vendedor');
+            $table->String('fechainicio');
+            $table->String('duracion');
+            $table->String('tipo');
+//laves
+            $table->foreign('patente')->references('patente')->on('vehiculos');
+            $table->foreign('vendedor')->references('nombre')->on('vendedores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polizas');
+    }
+}
