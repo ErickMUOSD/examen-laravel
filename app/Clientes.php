7a9992003a998269564a54c66e6269fd99f9d66d
2021 DSM33 Valeria Yamin Anastasio Mejia <al222010297@gmail.com>
2021-10-20 09:40:24 -0500

    Migracion clientes

---
+++ app/Clientes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class clientes extends Model
+{
+    public function vehiculos(){
+        return $this->belongsTo('vehiculos_clase');
+    }
+
+}
